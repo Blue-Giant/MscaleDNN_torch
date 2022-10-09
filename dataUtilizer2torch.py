9f8eb00caaf56a85d97dd78b06aab17172fbc7e5
Blue-Giant <42233512+Blue-Giant@users.noreply.github.com>
2022-10-10 00:16:24 +0800

    Add files via upload

--- dataUtilizer2torch.py
+++ dataUtilizer2torch.py
@@ -23,18 +23,19 @@ def rand_it(batch_size, variable_dim, region_a, region_b, to_torch=True, to_floa
     return x_it
 
 
-def rand_bd_1D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0):
+def rand_bd_1D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0,
+               use_grad2x=False):
     # np.asarray 将输入转为矩阵格式。
     # 当输入是列表的时候，更改列表的值并不会影响转化为矩阵的值
     # [0,1] 转换为 矩阵，然后
     # reshape(-1,1):数组新的shape属性应该要与原来的配套，如果等于-1的话，那么Numpy会根据剩下的维度计算出数组的另外一个shape属性值。
-    assert (variable_dim == 1)
+    assert (int(variable_dim) == 1)
 
     region_a = float(region_a)
     region_b = float(region_b)
 
-    x_left_bd = np.ones(shape=[batch_size, variable_dim], dtype=np.float32) * region_a
-    x_right_bd = np.ones(shape=[batch_size, variable_dim], dtype=np.float32) * region_b
+    x_left_bd = np.ones(shape=[batch_size, 1], dtype=np.float32) * region_a
+    x_right_bd = np.ones(shape=[batch_size, 1], dtype=np.float32) * region_b
     if to_float:
         x_left_bd = x_left_bd.astype(np.float32)
         x_right_bd = x_right_bd.astype(np.float32)
@@ -47,10 +48,14 @@ def rand_bd_1D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_f
             x_left_bd = x_left_bd.cuda(device='cuda:' + str(gpu_no))
             x_right_bd = x_right_bd.cuda(device='cuda:' + str(gpu_no))
 
+        x_left_bd.requires_grad = use_grad2x
+        x_right_bd.requires_grad = use_grad2x
+
     return x_left_bd, x_right_bd
 
 
-def rand_bd_2D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0):
+def rand_bd_2D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0,
+               use_grad=False):
     # np.asarray 将输入转为矩阵格式。
     # 当输入是列表的时候，更改列表的值并不会影响转化为矩阵的值
     # [0,1] 转换为 矩阵，然后
@@ -59,21 +64,15 @@ def rand_bd_2D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_f
     # np.random.random([100, 50]) 和 np.random.random((100, 50)) 效果一样
     region_a = float(region_a)
     region_b = float(region_b)
-    assert variable_dim == 2
+    assert (int(variable_dim) == 2)
     x_left_bd = (region_b-region_a) * np.random.random([batch_size, 2]) + region_a   # 浮点数都是从0-1中随机。
-    for ii in range(batch_size):
-        x_left_bd[ii, 0] = region_a
-
     x_right_bd = (region_b - region_a) * np.random.random([batch_size, 2]) + region_a
-    for ii in range(batch_size):
-        x_right_bd[ii, 0] = region_b
-
     y_bottom_bd = (region_b - region_a) * np.random.random([batch_size, 2]) + region_a
-    for ii in range(batch_size):
-        y_bottom_bd[ii, 1] = region_a
-
     y_top_bd = (region_b - region_a) * np.random.random([batch_size, 2]) + region_a
     for ii in range(batch_size):
+        x_left_bd[ii, 0] = region_a
+        x_right_bd[ii, 0] = region_b
+        y_bottom_bd[ii, 1] = region_a
         y_top_bd[ii, 1] = region_b
 
     if to_float:
@@ -92,156 +91,171 @@ def rand_bd_2D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_f
             y_bottom_bd = y_bottom_bd.cuda(device='cuda:' + str(gpu_no))
             y_top_bd = y_top_bd.cuda(device='cuda:' + str(gpu_no))
 
+        x_left_bd.requires_grad = use_grad
+        x_right_bd.requires_grad = use_grad
+        y_bottom_bd.requires_grad = use_grad
+        y_top_bd.requires_grad = use_grad
+
     return x_left_bd, x_right_bd, y_bottom_bd, y_top_bd
 
 
-def rand_bd_3D(batch_size, variable_dim, region_a, region_b):
+def rand_bd_3D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0,
+               use_grad=False):
     # np.asarray 将输入转为矩阵格式。
     # 当输入是列表的时候，更改列表的值并不会影响转化为矩阵的值
     # [0,1] 转换为 矩阵，然后
     # reshape(-1,1):数组新的shape属性应该要与原来的配套，如果等于-1的话，那么Numpy会根据剩下的维度计算出数组的另外一个shape属性值。
     region_a = float(region_a)
     region_b = float(region_b)
-    assert (variable_dim == 3)
+    assert (int(variable_dim) == 3)
 
     bottom_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
-    for ii in range(batch_size):
-        bottom_bd[ii, 2] = region_a
-
     top_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
-    for ii in range(batch_size):
-        top_bd[ii, 2] = region_b
-
     left_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
-    for ii in range(batch_size):
-        left_bd[ii, 1] = region_a
-
     right_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
-    for ii in range(batch_size):
-        right_bd[ii, 1] = region_b
-
     front_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
-    for ii in range(batch_size):
-        front_bd[ii, 0] = region_b
-
     behind_bd = (region_b - region_a) * np.random.rand(batch_size, 3) + region_a
     for ii in range(batch_size):
+        bottom_bd[ii, 2] = region_a
+        top_bd[ii, 2] = region_b
+        left_bd[ii, 1] = region_a
+        right_bd[ii, 1] = region_b
+        front_bd[ii, 0] = region_b
         behind_bd[ii, 0] = region_a
 
+    if to_float:
         bottom_bd = bottom_bd.astype(np.float32)
         top_bd = top_bd.astype(np.float32)
         left_bd = left_bd.astype(np.float32)
         right_bd = right_bd.astype(np.float32)
         front_bd = front_bd.astype(np.float32)
         behind_bd = behind_bd.astype(np.float32)
+    if to_torch:
+        bottom_bd = torch.from_numpy(bottom_bd)
+        top_bd = torch.from_numpy(top_bd)
+        left_bd = torch.from_numpy(left_bd)
+        right_bd = torch.from_numpy(right_bd)
+        front_bd = torch.from_numpy(front_bd)
+        behind_bd = torch.from_numpy(behind_bd)
+
+        if to_cuda:
+            bottom_bd = bottom_bd.cuda(device='cuda:' + str(gpu_no))
+            top_bd = top_bd.cuda(device='cuda:' + str(gpu_no))
+            left_bd = left_bd.cuda(device='cuda:' + str(gpu_no))
+            right_bd = right_bd.cuda(device='cuda:' + str(gpu_no))
+            front_bd = front_bd.cuda(device='cuda:' + str(gpu_no))
+            behind_bd = behind_bd.cuda(device='cuda:' + str(gpu_no))
+
+        bottom_bd.requires_grad = use_grad
+        top_bd.requires_grad = use_grad
+        left_bd.requires_grad = use_grad
+        right_bd.requires_grad = use_grad
+        front_bd.requires_grad = use_grad
+        behind_bd.requires_grad = use_grad
 
     return bottom_bd, top_bd, left_bd, right_bd, front_bd, behind_bd
 
 
-def rand_bd_4D(batch_size, variable_dim, region_a, region_b):
+def rand_bd_4D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0,
+               use_grad=False):
     # np.asarray 将输入转为矩阵格式。
     # 当输入是列表的时候，更改列表的值并不会影响转化为矩阵的值
     # [0,1] 转换为 矩阵，然后
     # reshape(-1,1):数组新的shape属性应该要与原来的配套，如果等于-1的话，那么Numpy会根据剩下的维度计算出数组的另外一个shape属性值。
     region_a = float(region_a)
     region_b = float(region_b)
-    variable_dim = int(variable_dim)
+    assert(int(variable_dim) == 4)
 
-    x0a = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
+    x0a = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x0b = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x1a = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x1b = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x2a = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x2b = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x3a = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
+    x3b = (region_b - region_a) * np.random.rand(batch_size, 4) + region_a
     for ii in range(batch_size):
         x0a[ii, 0] = region_a
-
-    x0b = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x0b[ii, 0] = region_b
-
-    x1a = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x1a[ii, 1] = region_a
-
-    x1b = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x1b[ii, 1] = region_b
-
-    x2a = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x2a[ii, 2] = region_a
-
-    x2b = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x2b[ii, 2] = region_b
-
-    x3a = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x3a[ii, 3] = region_a
-
-    x3b = (region_b - region_a) * np.random.rand(batch_size, variable_dim) + region_a
-    for ii in range(batch_size):
         x3b[ii, 3] = region_b
 
-    x0a = x0a.astype(np.float32)
-    x0b = x0b.astype(np.float32)
+    if to_float:
+        x0a = x0a.astype(np.float32)
+        x0b = x0b.astype(np.float32)
 
-    x1a = x1a.astype(np.float32)
-    x1b = x1b.astype(np.float32)
+        x1a = x1a.astype(np.float32)
+        x1b = x1b.astype(np.float32)
 
-    x2a = x2a.astype(np.float32)
-    x2b = x2b.astype(np.float32)
+        x2a = x2a.astype(np.float32)
+        x2b = x2b.astype(np.float32)
 
-    x3a = x3a.astype(np.float32)
-    x3b = x3b.astype(np.float32)
+        x3a = x3a.astype(np.float32)
+        x3b = x3b.astype(np.float32)
+
+    if to_torch:
+        x0a = torch.from_numpy(x0a)
+        x0b = torch.from_numpy(x0b)
+        x1a = torch.from_numpy(x1a)
+        x1b = torch.from_numpy(x1b)
+        x2a = torch.from_numpy(x2a)
+        x2b = torch.from_numpy(x2b)
+        x3a = torch.from_numpy(x3a)
+        x3b = torch.from_numpy(x3b)
+
+        if to_cuda:
+            x0a = x0a.cuda(device='cuda:' + str(gpu_no))
+            x0b = x0b.cuda(device='cuda:' + str(gpu_no))
+            x1a = x1a.cuda(device='cuda:' + str(gpu_no))
+            x1b = x1b.cuda(device='cuda:' + str(gpu_no))
+            x2a = x2a.cuda(device='cuda:' + str(gpu_no))
+            x2b = x2b.cuda(device='cuda:' + str(gpu_no))
+            x3a = x3a.cuda(device='cuda:' + str(gpu_no))
+            x3b = x3b.cuda(device='cuda:' + str(gpu_no))
 
     return x0a, x0b, x1a, x1b, x2a, x2b, x3a, x3b
 
 
-def rand_bd_5D(batch_size, variable_dim, region_a, region_b):
+def rand_bd_5D(batch_size, variable_dim, region_a, region_b, to_torch=True, to_float=True, to_cuda=False, gpu_no=0,
+               use_grad=False):
     # np.asarray 将输入转为矩阵格式。
     # 当输入是列表的时候，更改列表的值并不会影响转化为矩阵的值
     # [0,1] 转换为 矩阵，然后
     # reshape(-1,1):数组新的shape属性应该要与原来的配套，如果等于-1的话，那么Numpy会根据剩下的维度计算出数组的另外一个shape属性值。
     region_a = float(region_a)
     region_b = float(region_b)
-    if variable_dim == 5:
-        x0a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x0a[ii, 0] = region_a
+    assert variable_dim == 5
+    x0a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x0b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x1a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x1b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x2a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x2b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x3a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x3b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x4a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    x4b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
+    for ii in range(batch_size):
+        x0a[ii, 0] = region_a
+        x0b[ii, 0] = region_b
 
-        x0b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x0b[ii, 0] = region_b
+        x1a[ii, 1] = region_a
+        x1b[ii, 1] = region_b
 
-        x1a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x1a[ii, 1] = region_a
+        x2a[ii, 2] = region_a
+        x2b[ii, 2] = region_b
 
-        x1b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x1b[ii, 1] = region_b
+        x3a[ii, 3] = region_a
+        x3b[ii, 3] = region_b
 
-        x2a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x2a[ii, 2] = region_a
+        x4a[ii, 4] = region_a
+        x4b[ii, 4] = region_b
 
-        x2b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x2b[ii, 2] = region_b
-
-        x3a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x3a[ii, 3] = region_a
-
-        x3b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x3b[ii, 3] = region_b
-
-        x4a = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x4a[ii, 4] = region_a
-
-        x4b = (region_b - region_a) * np.random.rand(batch_size, 5) + region_a
-        for ii in range(batch_size):
-            x4b[ii, 4] = region_b
-
+    if to_float:
         x0a = x0a.astype(np.float32)
         x0b = x0b.astype(np.float32)
 
@@ -256,6 +270,29 @@ def rand_bd_5D(batch_size, variable_dim, region_a, region_b):
 
         x4a = x4a.astype(np.float32)
         x4b = x4b.astype(np.float32)
-        return x0a, x0b, x1a, x1b, x2a, x2b, x3a, x3b, x4a, x4b
-    else:
-        return
+
+    if to_torch:
+        x0a = torch.from_numpy(x0a)
+        x0b = torch.from_numpy(x0b)
+        x1a = torch.from_numpy(x1a)
+        x1b = torch.from_numpy(x1b)
+        x2a = torch.from_numpy(x2a)
+        x2b = torch.from_numpy(x2b)
+        x3a = torch.from_numpy(x3a)
+        x3b = torch.from_numpy(x3b)
+        x4a = torch.from_numpy(x4a)
+        x4b = torch.from_numpy(x4b)
+
+        if to_cuda:
+            x0a = x0a.cuda(device='cuda:' + str(gpu_no))
+            x0b = x0b.cuda(device='cuda:' + str(gpu_no))
+            x1a = x1a.cuda(device='cuda:' + str(gpu_no))
+            x1b = x1b.cuda(device='cuda:' + str(gpu_no))
+            x2a = x2a.cuda(device='cuda:' + str(gpu_no))
+            x2b = x2b.cuda(device='cuda:' + str(gpu_no))
+            x3a = x3a.cuda(device='cuda:' + str(gpu_no))
+            x3b = x3b.cuda(device='cuda:' + str(gpu_no))
+            x4a = x4a.cuda(device='cuda:' + str(gpu_no))
+            x4b = x4b.cuda(device='cuda:' + str(gpu_no))
+
+    return x0a, x0b, x1a, x1b, x2a, x2b, x3a, x3b, x4a, x4b
